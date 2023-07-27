92e8160d0e2d8b6a7b34369c9de56b0b532b87a6
Sabrina C. Sales <sacsales@icloud.com>
2023-07-27 10:43:19 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Sabrina C. Sales on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
